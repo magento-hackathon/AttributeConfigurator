7329cf2b7ccd596b4456ac7e5d272b64f3e77b3b
Rico Neitzel <rico@buro71a.de>
2014-05-10 15:58:29 +0200

    Versioncheck Preparation

--- src/app/code/community/Hackathon/AttributeConfigurator/Model/Observer.php
+++ src/app/code/community/Hackathon/AttributeConfigurator/Model/Observer.php
@@ -3,14 +3,38 @@
 /**
  * Class Hackathon_AttributeConfigurator_Model_Observer
  */
-
 class Hackathon_AttributeConfigurator_Model_Observer
 {
-    /**
-     *
-     */
-    public function checkAttributeVersion()
-    {
+    const XML_PATH_FILENAME = 'catalog/attribute_configurator/product_xml_location';
+    const XML_PATH_CURRENT_HASH = 'attributeconfigurator/hashes/current';
 
+    protected $_helper;
+
+    public function __construct()
+    {
+        $this->_helper = Mage::helper('hackathon_attributeconfigurator');
+    }
+
+    public function controllerActionPredispatchAdminhtml(Varien_Event_Observer $observer)
+    {
+        $this->isAttributeXmlNewer();
+    }
+
+    /**
+     * Check if the XML file is newer than the last imported one.
+     *
+     * return bool
+     */
+    protected function isAttributeXmlNewer()
+    {
+        $filename        = Mage::getConfig()->getNode(self::XML_PATH_FILENAME);
+        $currentFileHash = Mage::getStoreConfigFlag(self::XML_PATH_CURRENT_HASH);
+        $latestFileHash  = $this->_helper->createFileHash($filename);
+
+        if ($latestFileHash !== $currentFileHash) {
+            return true;
+        }
+
+        return false;
     }
 }
