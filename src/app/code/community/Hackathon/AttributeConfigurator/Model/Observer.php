b47e8bfe798092028191bba05d45831a09cda0fd
Rico Neitzel <rico@buro71a.de>
2014-05-10 14:03:05 +0200

    CodeFormatting

--- src/app/code/community/Hackathon/AttributeConfigurator/Model/Observer.php
+++ src/app/code/community/Hackathon/AttributeConfigurator/Model/Observer.php
@@ -1,12 +1,16 @@
 <?php
+
 /**
  * Class Hackathon_AttributeConfigurator_Model_Observer
  */
-class Hackathon_AttributeConfigurator_Model_Observer {
+
+class Hackathon_AttributeConfigurator_Model_Observer
+{
     /**
      *
      */
-    public function checkAttributeVersion(){
+    public function checkAttributeVersion()
+    {
 
     }
 }
