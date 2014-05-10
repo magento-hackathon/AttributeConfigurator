e6bfa3c959e5a015c46683787659e47d445e7c4b
ikkaan <joachim.adomeit@nidag.com>
2014-05-10 12:25:51 +0200

    Adding Update Stub, Observer Stub

---
+++ src/app/code/community/Hackathon/AttributeConfigurator/Model/Hackathon_AttributeConfigurator_Model_Observer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Class Hackathon_AttributeConfigurator_Helper_Data
+ */
+class Hackathon_AttributeConfigurator_Model_Observer {
+    /**
+     *
+     */
+    public function checkAttributeVersion(){
+
+    }
+}
