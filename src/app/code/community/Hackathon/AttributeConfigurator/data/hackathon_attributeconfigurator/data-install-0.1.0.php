926e37a1ec07d3390c1bdbdd35444ffe087ad3af
ikkaan <joachim.adomeit@nidag.com>
2014-05-11 13:01:58 +0200

    Fixed Typo

--- src/app/code/community/Hackathon/AttributeConfigurator/data/hackathon_attributeconfigurator/data-install-0.1.0.php
+++ src/app/code/community/Hackathon/AttributeConfigurator/data/hackathon_attributeconfigurator/data-install-0.1.0.php
@@ -6,7 +6,7 @@ $installer->startSetup();
 try{
     $installer->run("ALTER TABLE eav_attribute ADD COLUMN is_maintained_by_configurator smallint(5)");
 }catch (Exception $e){
-    Mage::exception('hackathon_attributeconfigurator data upgrad exception: '.$e->getMessage());
+    Mage::exception('hackathon_attributeconfigurator data upgrade exception: '.$e->getMessage());
 }
 
 $installer->endSetup();
