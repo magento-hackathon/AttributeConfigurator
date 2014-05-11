f16613b58f28de96bca1c1a8a9c00b3856d97603
ikkaan <joachim.adomeit@nidag.com>
2014-05-11 13:01:07 +0200

    Code Formatting, adding doc

--- src/app/code/community/Hackathon/AttributeConfigurator/Model/Sync/Import.php
+++ src/app/code/community/Hackathon/AttributeConfigurator/Model/Sync/Import.php
@@ -1,12 +1,12 @@
 <?php
-
+/**
+ * Class Hackathon_AttributeConfigurator_Model_Sync_Import
+ */
 class Hackathon_AttributeConfigurator_Model_Sync_Import extends Mage_Core_Model_Abstract
 {
-
     /** @var Hackathon_AttributeConfigurator_Helper_Data $_helper */
     protected $_helper;
 
-   
     /**
      * Attribute Data
      * @var array
@@ -22,10 +22,9 @@ class Hackathon_AttributeConfigurator_Model_Sync_Import extends Mage_Core_Model_
     protected $_groupData = array();
 
 
- public function _construct()
+    public function _construct()
     {
         $this->_helper = Mage::helper('hackathon_attributeconfigurator/data');
-
         $this->bibiBlocksberg();
     }
 
@@ -35,61 +34,75 @@ class Hackathon_AttributeConfigurator_Model_Sync_Import extends Mage_Core_Model_
      *
      * return bool
      */
-
     public function import()
     {
         $_config = Mage::getConfig();
 
-
         // 1. Import/Delete Attribute Sets
         $attributesets = $_config->getNode('attributesetslist');
-
 
         // 2. Import/Delete Attributes
         $attributes = $_config->getNode('attributeslist');
 
         if ($this->_validate($attributesets, $attributes)) {
-
             // 3. Connect Attributes with Attribute Sets using Attribute Groups
         }
-
     }
 
-
+    /**
+     * Get Attribute Set from XML
+     *
+     * @param $xml
+     * @return $this
+     */
     public function prepareAttributeSet($xml)
     {
         $this->_setData = json_decode(json_encode($xml->attributesets), true);
         return $this;
     }
 
+    /**
+     * Parse XML for Attribute Set
+     *
+     * @param $attributesets
+     * @return array
+     */
     protected function _getAttributeSetsFromXml($attributesets)
     {
         $returnarray = array();
         foreach ($attributesets->children() as $attributeset) {
             $returnarray[] = (string) $attributeset['name'];
         }
-
         return $returnarray;
     }
 
-
-
     /**
+     * Fetch Attributes from XML
+     *
      * @param $xml
      * @return $this
      */
     public function prepareAttributes($xml)
     {   
-     $this->_attributeData = json_decode(json_encode($xml->attributes), true);
+        $this->_attributeData = json_decode(json_encode($xml->attributes), true);
         return $this;
-        }
-	/** @TODO: RICO schön machen und weitermachen :D **/
+    }
+
+    /**
+     * Validate Attributesets and Attributes
+     *
+     * @TODO: RICO schön machen und weitermachen :D
+     *
+     * @param $attributesets
+     * @param $attributes
+     * @return bool
+     * @throws Mage_Adminhtml_Exception
+     */
     protected function _validate($attributesets, $attributes)
     {
-
         $attributesets = $attributesets;
         $lo_attributesets = $this->_getAttributeSetsFromXml($attributesets);
-        $attributes    = $attributes;
+        $attributes = $attributes;
 
         foreach ($attributes->children() as $attribute) {
             foreach ($attribute->attributesets->children() as $attributeset) {
@@ -97,23 +110,31 @@ class Hackathon_AttributeConfigurator_Model_Sync_Import extends Mage_Core_Model_
             }
         }
 
-
-
-                if(!in_array($attributeset["name"], $lo_attributesets)){
-                    throw new Mage_Adminhtml_Exception("Attributeset '".$attributeset["name"]."' referenced by '".$attribute["code"]."' is not listed in the attributesetslist element");
-                }
+        if (!in_array($attributeset["name"], $lo_attributesets)) {
+            throw new Mage_Adminhtml_Exception("Attributeset '".$attributeset["name"]."' referenced by '".$attribute["code"]."' is not listed in the attributesetslist element");
+        }
 
         foreach ($attributesets->children() as $attributeset) {
             //echo $attributeset['name'] . "<br />";
         }
-
         return false;
-}
-   public function prepareAttributeGroups($xml)
+    }
+
+    /**
+     * Fetches Attribute Groups from XML
+     *
+     * @param $xml
+     * @return $this
+     */
+    public function prepareAttributeGroups($xml)
     {
         $this->_groupData = json_decode(json_encode($xml->attributegroups), true);
-        return $this;    }
+        return $this;
+    }
 
+    /**
+     * Does 'magic' ?
+     */
     protected function bibiBlocksberg()
     {
         Mage::getConfig()->loadFile($this->_helper->getImportFilename());
