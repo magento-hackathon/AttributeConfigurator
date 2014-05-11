847fa5a0529cd74cf0aa062a0c95c4c1f0897f64
nils.preuss <nils.preuss@gmx.net>
2014-05-11 10:07:31 +0200

    Added xml processing to have the needed information in a more usable format.
    This still needs improvement to be really usefull because for
    maintenance and human readability the actual xml structure is way more
    usefull.

--- src/app/code/community/Hackathon/AttributeConfigurator/Model/Sync/Import.php
+++ src/app/code/community/Hackathon/AttributeConfigurator/Model/Sync/Import.php
@@ -2,6 +2,21 @@
 
 class Hackathon_AttributeConfigurator_Model_Sync_Import extends Mage_Core_Model_Abstract {
 
+    /**
+     * Attribute Data
+     * @var array
+     */
+    protected $_attributeData = array();
+
+    /**
+     * Attribute-Set Data
+     * @var array
+     */
+    protected $_setData = array();
+
+    protected $_groupData = array();
+
+
     public function _construct(){
         $this->bibiBlocksberg();
     }
@@ -27,5 +42,28 @@ class Hackathon_AttributeConfigurator_Model_Sync_Import extends Mage_Core_Model_
 
     private function bibiBlocksberg(){
         Mage::getConfig()->loadFile(Hackathon_AttributeConfigurator_Model_Observer::XML_PATH_FILENAME);
+    }
+
+    public function prepareAttributeSet($xml)
+    {
+        $this->_setData = json_decode(json_encode($xml->attributesets), true);
+        return $this;
+    }
+
+
+    /**
+     * @param $xml
+     * @return $this
+     */
+    public function prepareAttributes($xml)
+    {
+        $this->_attributeData = json_decode(json_encode($xml->attributes), true);
+        return $this;
+    }
+
+    public function prepareAttributeGroups($xml)
+    {
+        $this->_groupData = json_decode(json_encode($xml->attributegroups), true);
+        return $this;
     }
 }
