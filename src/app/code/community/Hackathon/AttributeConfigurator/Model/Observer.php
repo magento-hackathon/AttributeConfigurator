64f663f72bf7732624d51044aa221098e68023ce
Rico Neitzel <rico@buro71a.de>
2014-05-11 09:36:48 +0200

    Refactored Helper/Model to concentrate methods in helper. prepared structure for importing in model. validation for proper data nesting.

--- src/app/code/community/Hackathon/AttributeConfigurator/Model/Observer.php
+++ src/app/code/community/Hackathon/AttributeConfigurator/Model/Observer.php
@@ -5,14 +5,12 @@
  */
 class Hackathon_AttributeConfigurator_Model_Observer
 {
-    const XML_PATH_FILENAME = 'catalog/attribute_configurator/product_xml_location';
-    const XML_PATH_CURRENT_HASH = 'attributeconfigurator/hashes/current';
 
     protected $_helper;
 
     public function __construct()
     {
-        $this->_helper = Mage::helper('hackathon_attributeconfigurator');
+        $this->_helper = Mage::helper('hackathon_attributeconfigurator/data');
     }
 
     /**
@@ -23,26 +21,9 @@ class Hackathon_AttributeConfigurator_Model_Observer
      */
     public function controllerActionPredispatchAdminhtml(Varien_Event_Observer $observer)
     {
-        if($this->isAttributeXmlNewer()) {
+        if($this->_helper->isAttributeXmlNewer()) {
             Mage::getModel('hackathon_attributeconfigurator/sync_import')->import();
         }
     }
 
-    /**
-     * Check if the XML file is newer than the last imported one.
-     *
-     * return bool
-     */
-    protected function isAttributeXmlNewer()
-    {
-        $filename        = Mage::getStoreConfig(self::XML_PATH_FILENAME);
-        $currentFileHash = Mage::getStoreConfigFlag(self::XML_PATH_CURRENT_HASH);
-        $latestFileHash  = $this->_helper->createFileHash($filename);
-
-        if ($latestFileHash !== $currentFileHash) {
-            return true;
-        }
-
-        return false;
-    }
 }
--- src/app/code/community/Hackathon/AttributeConfigurator/Model/Sync/Import.php
+++ src/app/code/community/Hackathon/AttributeConfigurator/Model/Sync/Import.php
@@ -1,8 +1,14 @@
 <?php
 
-class Hackathon_AttributeConfigurator_Model_Sync_Import extends Mage_Core_Model_Abstract {
+class Hackathon_AttributeConfigurator_Model_Sync_Import extends Mage_Core_Model_Abstract
+{
 
-    public function _construct(){
+    /** @var Hackathon_AttributeConfigurator_Helper_Data $_helper */
+    protected $_helper;
+
+    public function _construct()
+    {
+        $this->_helper = Mage::helper('hackathon_attributeconfigurator/data');
         $this->bibiBlocksberg();
     }
 
@@ -13,19 +19,63 @@ class Hackathon_AttributeConfigurator_Model_Sync_Import extends Mage_Core_Model_
      * return bool
      */
 
-    public function import(){
-        // 1. Import/Delete Attribute Sets
+    public function import()
+    {
         $_config = Mage::getConfig();
 
+
+        // 1. Import/Delete Attribute Sets
         $attributesets = $_config->getNode('attributesetslist');
 
 
         // 2. Import/Delete Attributes
+        $attributes = $_config->getNode('attributeslist');
 
-        // 3. Connect Attributes with Attribute Sets using Attribute Groups
+        if ($this->_validate($attributesets, $attributes)) {
+
+            // 3. Connect Attributes with Attribute Sets using Attribute Groups
+        }
+
     }
 
-    private function bibiBlocksberg(){
-        Mage::getConfig()->loadFile(Hackathon_AttributeConfigurator_Model_Observer::XML_PATH_FILENAME);
+    protected function _getAttributeSetsFromXml($attributesets)
+    {
+        $returnarray = array();
+        foreach ($attributesets->children() as $attributeset) {
+            $returnarray[] = (string) $attributeset['name'];
+        }
+
+        return $returnarray;
+
+    }
+
+
+	/** @TODO: RICO schön machen und weitermachen :D **/
+    protected function _validate($attributesets, $attributes)
+    {
+
+        $attributesets = $attributesets;
+        $lo_attributesets = $this->_getAttributeSetsFromXml($attributesets);
+        $attributes    = $attributes;
+
+        foreach ($attributes->children() as $attribute) {
+            foreach ($attribute->attributesets->children() as $attributeset) {
+                //echo $attribute["code"] . " gehört zu " . $attributeset["name"] . " <br />";
+                if(!in_array($attributeset["name"], $lo_attributesets)){
+                    throw new Mage_Adminhtml_Exception("Attributeset '".$attributeset["name"]."' referenced by '".$attribute["code"]."' is not listed in the attributesetslist element");
+                }
+            }
+        }
+
+        foreach ($attributesets->children() as $attributeset) {
+            //echo $attributeset['name'] . "<br />";
+        }
+
+        return false;
+    }
+
+    protected function bibiBlocksberg()
+    {
+        Mage::getConfig()->loadFile($this->_helper->getImportFilename());
     }
 }
