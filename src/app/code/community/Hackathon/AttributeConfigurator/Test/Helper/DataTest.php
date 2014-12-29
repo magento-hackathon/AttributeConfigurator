b113e3ef74075e8c5971ed0231e78eca8a9ad4d4
Stefan Schwan <stefan.schwan@gmail.com>
2014-12-29 13:40:24 +0100

    added missing tests for the data helper

--- src/app/code/community/Hackathon/AttributeConfigurator/Test/Helper/DataTest.php
+++ src/app/code/community/Hackathon/AttributeConfigurator/Test/Helper/DataTest.php
@@ -4,32 +4,91 @@
  */
 class Hackathon_AttributeConfigurator_Test_Helper_DataTest extends EcomDev_PHPUnit_Test_Case
 {
-    /** @var Hackathon_AttributeConfigurator_Helper_Data $_helper*/
+    /** @var Hackathon_AttributeConfigurator_Helper_Data An instance of the helper*/
     protected $_helper;
+
+    /**
+     * @var string Location of the file used for testing
+     */
+    protected $_testFile;
 
     protected function setUp()
     {
-        $this->_helper = MAGE::helper('hackathon_attributeconfigurator');
-        parent::setUp();
-    }
-
-    public function testCreateFileHash()
-    {
-        /** @var string $fileHash */
-        $fileHash = '39e261858ae67d3aed716969e449686a';
-        /** @var string $testFile */
-        $testFileName = Mage::getModuleDir('', 'Hackathon_AttributeConfigurator') .
+        $this->_helper = Mage::helper('hackathon_attributeconfigurator');
+        $this->_testFile = Mage::getModuleDir('', 'Hackathon_AttributeConfigurator') .
                 DS . 'Test' . DS . 'Helper' . DS . 'Fixture' . DS . 'attribute-dummy.xml' ;
-
-        $this->assertEquals($fileHash, $this->_helper->createFileHash($testFileName));
-        $this->assertFalse($this->_helper->createFileHash(''));
-        $this->assertFalse($this->_helper->createFileHash('ranD0MsTr1ng'));
+        parent::setUp();
     }
 
     public function testGetImportFilename()
     {
         $this->assertNotNull(Mage::getStoreConfig(Hackathon_AttributeConfigurator_Helper_Data::XML_PATH_FILENAME));
         $this->assertInternalType('int', strpos($this->_helper->getImportFilename(), Mage::getBaseDir() . DS ));
+    }
+
+    public function testCreateFileHash()
+    {
+        $this->assertNotFalse(
+                $this->_helper->createFileHash(
+                        $this->_testFile
+                )
+        );
+    }
+
+    public function testIsAttributeXmlNewer()
+    {
+
+
+
+        //get the current value of the config - should be null, but will be restored after testing just in case
+        $originalConfigValue = Mage::getStoreConfig(Hackathon_AttributeConfigurator_Helper_Data::XML_PATH_CURRENT_HASH);
+
+
+
+
+        //Set up a reflection class to change the filename to an abitrary value
+        $helperReflection = new ReflectionClass('Hackathon_AttributeConfigurator_Helper_Data');
+        $helperInstance = $helperReflection->newInstance();
+
+        $fileNameProperty = $helperReflection->getProperty('_importFilename');
+        $fileNameProperty->setAccessible(true);
+
+        $fileNameProperty->setValue( $helperInstance, $this->_testFile);
+
+
+        //set the config to the test file's hash
+        Mage::getModel('core/config')
+                ->saveConfig(
+                        Hackathon_AttributeConfigurator_Helper_Data::XML_PATH_CURRENT_HASH,
+                        md5_file($this->_testFile)
+                );
+        //never forget to reset the config after applying changes!!!
+        Mage::app()->getStore()->resetConfig();
+
+        $this->assertFalse(
+                $helperInstance->isAttributeXmlNewer()
+        );
+
+        //set the config to a different value
+        Mage::getModel('core/config')
+            ->saveConfig(
+                Hackathon_AttributeConfigurator_Helper_Data::XML_PATH_CURRENT_HASH,
+                "testValue"
+        );
+        Mage::app()->getStore()->resetConfig();
+
+        $this->assertTrue(
+                $helperInstance->isAttributeXmlNewer()
+        );
+
+        //revert the config to it's orginal state
+        Mage::getModel('core/config')
+                ->saveConfig(
+                        Hackathon_AttributeConfigurator_Helper_Data::XML_PATH_CURRENT_HASH,
+                        $originalConfigValue
+                );
+        Mage::app()->getStore()->resetConfig();
+
     }
 
     public function testcheckAttributeMaintained()
@@ -40,4 +99,6 @@ class Hackathon_AttributeConfigurator_Test_Helper_DataTest extends EcomDev_PHPUn
         $this->assertFalse($this->_helper->checkAttributeMaintained($attribute->setIsMaintainedByConfigurator(0)));
         $this->assertFalse($this->_helper->checkAttributeMaintained(NULL));
     }
+
+
 }
